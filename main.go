547706a31cae35dc9a11e7339281541efc17bf13
k175jp <tai.sp.tai@gmail.com>
2023-12-19 13:33:10 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "net/http"
+    "github.com/labstack/echo"
+)
+
+func main() {
+    e := echo.New()
+
+    e.GET("/", Hello())
+    e.GET("/api/", ApiHelloGet())
+
+    e.Start(":8080")
+}
+
+func Hello() echo.HandlerFunc {
+    return func(c echo.Context) error {
+        return c.String(http.StatusOK, "4439: GoはGoogleが開発した言語 ver.2")
+    }
+}
+
+func ApiHelloGet() echo.HandlerFunc {
+    return func(c echo.Context) error {
+        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4439", "message": "GoはGoogleが開発した言語"})
+    }
+}
