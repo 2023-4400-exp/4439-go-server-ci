8c865d0a2707cffbc90c10dee484aedfb8c92654
k175jp <tai.sp.tai@gmail.com>
2024-01-09 13:56:20 +0900

    [update] messageの変更

--- main.go
+++ main.go
@@ -16,12 +16,12 @@ func main() {
 
 func Hello() echo.HandlerFunc {
     return func(c echo.Context) error {
-        return c.String(http.StatusOK, "4439: GoはGoogleが開発した言語!!!")
+        return c.String(http.StatusOK, "4439: GoはGoogleが開発した言語!?")
     }
 }
 
 func ApiHelloGet() echo.HandlerFunc {
     return func(c echo.Context) error {
-        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4439", "message": "GoはGoogleが開発した言語!!!"})
+        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4439", "message": "GoはGoogleが開発した言語!?"})
     }
 }
